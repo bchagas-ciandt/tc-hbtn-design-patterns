cb758d716f01ee2a5eb3c38025b019f0066e9552
bchagas99 <bchagas@ciandt.com>
2022-08-15 23:43:10 -0300

    subindo subrepo 0x00/personagem

---
+++ 0x00/personagem/Guerreiro.java
@@ -0,0 +1,19 @@
+public class Guerreiro extends Personagem{
+
+    public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        super(nome, TipoPersonagem.GUERREIRO, inteligencia, forca, vigor, resistencia, destreza);
+        if (forca < inteligencia || forca < destreza) {
+            throw new IllegalArgumentException("Atributos invalidos para GUERREIRO");
+        };
+    }
+
+    @Override
+    public double getDanoAtaque() {
+        return (inteligencia * 0.05) + (forca * 0.8) + (destreza * 0.1) + (vigor * 0.05);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ 0x00/personagem/Personagem.java
@@ -0,0 +1,26 @@
+public abstract class Personagem {
+    protected String nome;
+    protected TipoPersonagem tipo;
+    protected int inteligencia;
+    protected int forca;
+    protected int vigor;
+    protected int resistencia;
+    protected int destreza;
+
+    public Personagem(String nome, TipoPersonagem tipo, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.inteligencia = inteligencia;
+        this.forca = forca;
+        this.vigor = vigor;
+        this.resistencia = resistencia;
+        this.destreza = destreza;
+    }
+
+    public abstract double getDanoAtaque();
+
+    @Override
+    public String toString() {
+        return String.format("Personagem { nome = %s, tipo = %s, inteligencia = %d, forca = %d, vigor = %d, resistencia = %d, destreza = %d, dano ataque = %.2f }", nome, tipo, inteligencia, forca, vigor, resistencia, destreza, getDanoAtaque());
+    }
+}
