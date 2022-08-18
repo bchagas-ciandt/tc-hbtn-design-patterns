7dc7953973469bd7fa9657ec1b7527a3a11c745e
bchagas99 <bchagas@ciandt.com>
2022-08-18 14:49:35 -0300

    mudando de println para printf

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -2,10 +2,10 @@ public class AudioPlayer implements MediaPlayer{
     @Override
     public void reproduzir(TipoMedia tipo, String nome) {
         if(tipo == TipoMedia.MP3) {
-            System.out.println("Reproduzindo MP3: "+ nome);
+            System.out.printf("Reproduzindo MP3: %s", nome);
         }
         if (tipo == TipoMedia.VLC) {
-            System.out.println("Reproduzindo VLC: "+ nome);
+            System.out.printf("Reproduzindo VLC: %s", nome);
         }
     }
 }
--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -1,11 +1,11 @@
 public class VideoMediaPlayer implements AdvancedMediaPlayer{
     @Override
     public void reproduzirVlc(String nome) {
-        System.out.println("Reproduzindo VLC: "+ nome);
+        System.out.printf("Reproduzindo VLC: %s",nome);
     }
 
     @Override
     public void reproduzirMp4(String nome) {
-        System.out.println("Reproduzindo MP4: "+ nome);
+        System.out.printf("Reproduzindo MP4: %s", nome);
     }
 }
